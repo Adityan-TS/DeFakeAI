4e2efa8f6a86a4a5a892611712610df6bd92a47a
Adityan Thirumani Srinivasan <97823915+Adityan-TS@users.noreply.github.com>
2024-11-20 17:37:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,6 @@ def compute_accuracy(y_true, y_pred):
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=False)
+
